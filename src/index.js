961155389ee679c3bc7d88fc51357cd6dd61361a
Cujbă George Valentin <george.cujba15@gmail.com>
2022-10-09 00:25:13 +0300

    Add the Cards into a CardList component which is created by mapping over an array

--- src/index.js
+++ src/index.js
@@ -4,28 +4,12 @@ import './index.css';
 
 // -> this gives us access to already created CSS Classes
 import 'tachyons';
-import Card from "./app/components/Card";
+import CardList from "./app/components/CardList";
 import { robots } from "./app/utils/robots";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <Card 
-        id={robots[0].id}
-        name={robots[0].name}
-        email={robots[0].email}
-      />
-      <Card 
-        id={robots[1].id}
-        name={robots[1].name}
-        email={robots[1].email}
-      />
-      <Card 
-        id={robots[2].id}
-        name={robots[2].name}
-        email={robots[2].email}
-      />
-    </div>
+    <CardList robots={robots} />
   </React.StrictMode>
 );
