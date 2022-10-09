678ebfda000d25d8f1d90c0bd35c3bd78fc760e9
Cujbă George Valentin <george.cujba15@gmail.com>
2022-10-09 22:25:36 +0300

    Add styles

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
-import { robots } from "../app/utils/robots";
+import "./App.css";
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
     }
 
+    // -> this gets ran after render()
+    // -> if this changes state then render is ran again 
+    componentDidMount() {
+
+        // -> make an HTTP Request to the link to get
+        // data 
+        // -> save the data retrieved in state
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+    // -> it's good practice to use arrow functions for custom methods
+    // as react will be able to tell where to grab the "this" from
     onSearchChange = (event) => {
         // -> take what gets entered inside the input box and store it
         // inside the "searchField" state var 
@@ -25,9 +39,13 @@ class App extends Component {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         });
 
+        if(this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        }
+        
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots} />
             </div>
